5739af2c39619ba8f0e088bf4030c0d5307ad080
Jorge <jvazquez1992@outlook.com>
2022-02-25 17:53:27 -0500

    -Validacion min

--- src/main/java/devsu/challenge/bank/controller/ProductoController.java
+++ src/main/java/devsu/challenge/bank/controller/ProductoController.java
@@ -2,7 +2,8 @@ package devsu.challenge.bank.controller;
 
 import java.util.List;
 
-import org.springframework.data.repository.query.Param;
+import javax.validation.constraints.Min;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +31,7 @@ public class ProductoController {
 	}
 	
 	@PutMapping(value="/{id}/{stock}")
-	public ResponseEntity<ProductoDTO> actualizarStock(@PathVariable("id") Long id, @PathVariable("stock") Integer stock){
-		System.out.println(id);
-		System.out.println(stock);
+	public ResponseEntity<ProductoDTO> actualizarStock(@PathVariable("id") Long id,@Min(1) @PathVariable("stock") Integer stock){
 		ProductoDTO respuesta = productoService.actualizarStock(id, stock);
 		return new ResponseEntity<>(respuesta, HttpStatus.OK);
 	}
